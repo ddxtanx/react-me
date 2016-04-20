4121fd76b142a212caf5e0f7e79f7a8e767ba054
Garrett Credi <gcc@ameritech.net>
2016-04-20 15:13:43 -0500

    Disable scrolling

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ at gcc@ameritech.net and I will get back to you as soon as I can.
 Comments will be coming soon, once I don't have a lot going on with school.
 */
 // No Scrolling
-$(document).bind('touchmove', false);
+document.ontouchmove = function(event){
+    event.preventDefault();
+}
 // Variable initilazation
 
 // Workaround for 300ms delay
