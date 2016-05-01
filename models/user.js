761b2c041d3c52f407426d3f8f6eb679b16d7277
Garrett Credi <gcc@ameritech.net>
2016-05-01 13:57:57 -0500

    Add nodejs user database login functionality

---
+++ models/user.js
@@ -0,0 +1,57 @@
+var bcrypt = require('bcryptjs');
+// Use db.users.find().pretty()
+var mongoose = require('mongoose');
+
+var mongoURI = "mongodb://ddxtanx:1Bvbg6mjm%s@ds011872.mlab.com:11872/logins";
+var MongoDB = mongoose.connect(mongoURI).connection;
+MongoDB.on('error', function(err) { console.log(err.message); });
+MongoDB.once('open', function() {
+  console.log("mongodb connection open");
+});
+
+var db = mongoose.connection;
+
+// User Schema
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index: true
+	},
+	password: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	name: {
+		type: String
+	},
+	profileimage:{
+		type: String
+	}
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	callback(null, isMatch);
+	});
+}
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+    	bcrypt.hash(newUser.password, salt, function(err, hash) {
+   			newUser.password = hash;
+   			newUser.save(callback);
+    	});
+	});
+}
